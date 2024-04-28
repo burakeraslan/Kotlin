841d8a6d7f91d57461de6cb4a203c93a31b049c5
burak <buraakeraslan@outlook.com>
2024-04-28 21:28:39 +0300

    navigation

---
+++ KotlinNavigation/app/src/main/java/com/example/kotlinnavigation/FirstFragment.kt
@@ -0,0 +1,34 @@
+package com.example.kotlinnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.button).setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("burak")
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
